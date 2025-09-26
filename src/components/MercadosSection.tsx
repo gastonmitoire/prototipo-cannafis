b3a1039d863176822c02999e6e165731d8fbbc3e
Gaston Mitoire <gastonmm@gmail.com>
2025-09-26 05:21:00 -0300

    mercados 2 init

--- src/components/MercadosSection.tsx
+++ src/components/MercadosSection.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import type { ReactNode } from "react";
 import { motion } from "framer-motion";
 import { useInView as useInViewLib } from "framer-motion";
+import Title from "./ui/title";
 interface MercadosSectionProps {
   sectionRef: React.RefObject<HTMLElement | null>;
   fadeUp: any;
@@ -19,7 +20,7 @@ const MercadosCard: React.FC<MercadosCardProps> = ({ text, icon }) => {
   return (
     <div
       ref={ref}
-      className="bg-black/30 backdrop-blur-xl rounded-lg px-6 py-16 text-xl text-white/90 mb-4 shadow-lg flex flex-col items-center"
+      className="bg-black/30 backdrop-blur-xl rounded-lg px-6 py-32 text-white/90 mb-4 shadow-lg flex flex-col items-center"
     >
       {icon && (
         <motion.span
@@ -36,7 +37,7 @@ const MercadosCard: React.FC<MercadosCardProps> = ({ text, icon }) => {
             mass: 0.7,
             delay: 0.05,
           }}
-          className="mb-2 text-5xl drop-shadow"
+          className="mb-5 text-5xl drop-shadow"
         >
           {icon}
         </motion.span>
@@ -45,7 +46,7 @@ const MercadosCard: React.FC<MercadosCardProps> = ({ text, icon }) => {
         initial={{ opacity: 0, y: 18 }}
         animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 18 }}
         transition={{ duration: 0.48, delay: 0.13, ease: [0.22, 1, 0.36, 1] }}
-        className="block text-center font-semibold"
+        className="block text-center text-3xl font-thin"
       >
         {text}
       </motion.span>
@@ -78,24 +79,11 @@ const MercadosSection: React.FC<MercadosSectionProps> = ({
     <section
       id="mercados"
       ref={sectionRef}
-      className="relative h-screen flex items-center justify-center bg-gradient-to-br from-white/10 via-transparent to-cyan-200/10 px-4"
-      style={{
-        backgroundImage:
-          'url("https://www.transparenttextures.com/patterns/classy-fabric.png")',
-        backgroundPosition: "center",
-        backgroundAttachment: "fixed",
-      }}
+      className="h-screen flex items-center justify-center bg-gradient-to-br from-slate-900/30 via-blue-950/70 to-slate-900/70 border-y border-blue-900/40 px-4"
     >
       <div className="flex flex-col items-center w-full z-10">
-        <div className="w-full max-w-6xl">
-          <motion.h2
-            variants={fadeUp}
-            initial="hidden"
-            animate={inView ? "show" : "hidden"}
-            className="text-3xl font-bold mb-4 text-white/80 text-center uppercase"
-          >
-            Mercados y alianzas
-          </motion.h2>
+        <div className="container w-full">
+          <Title>Mercados y alianzas</Title>
           <div className="grid grid-cols-3 gap-3 mt-6">
             {mercadosData.map((item, key) => (
               <MercadosCard
