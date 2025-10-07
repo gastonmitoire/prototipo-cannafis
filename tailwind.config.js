129f12716ad3c2843bfd2876510235963237b068
Gaston Mitoire <gastonmm@gmail.com>
2025-10-07 15:18:05 -0300

    brand colors palette init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Paleta funcional
+        background: "#001F3E", // Midnight Blue
+        foreground: "#E9EAE1", // Pearl
+        primary: "#05D16E", // Malachite
+        secondary: "#00804C", // Leaf Green
+        accent: "#1E4990", // Nuit
+
+        // Paleta de marca (opcional si querés usar por nombre también)
+        midnight: "#001F3E",
+        malachite: "#05D16E",
+        pearl: "#E9EAE1",
+        nuit: "#1E4990",
+        "leaf-green": "#00804C",
+      },
+    },
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1440px', // igualar al header si es necesario
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
       },
     },
   },
